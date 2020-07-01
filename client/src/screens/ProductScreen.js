085c00c73355a5c87dd978ff29c7979dc28eec75
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-01 09:40:08 +0530

    ProductScreen complete

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
+	const [qty, setQty] = useState(1);
 	const productDetails = useSelector((state) => state.productDetails);
 	const { product, loading, error } = productDetails;
 	const dispatch = useDispatch();
+
+	const handleAddToCart = () => {
+		props.history.push("/cart/" + props.match.params.id + "?qty=" + qty);
+	};
 
 	useEffect(() => {
 		dispatch(detailsProduct(props.match.params.id));
@@ -14,7 +19,65 @@ function ProductScreen(props) {
 	if (loading) return <h1>Loading...</h1>;
 	if (error) return <h1>Error</h1>;
 	console.log(product);
-	return <h1>Product</h1>;
+	return (
+		<div>
+			<div className="back-to-result">
+				<Link to="/">Back to result</Link>
+			</div>
+			<div className="details">
+				<div className="details-image">
+					<img src={product.image} alt="product"></img>
+				</div>
+				<div className="details-info">
+					<ul>
+						<li>
+							<h4>{product.name}</h4>
+						</li>
+						<li>
+							{product.rating} Stars ({product.numReviews} Reviews)
+						</li>
+						<li>
+							Price: <b>${product.price}</b>
+						</li>
+						<li>
+							Description:
+							<div>{product.description}</div>
+						</li>
+					</ul>
+				</div>
+				<div className="details-action">
+					<ul>
+						<li>Price: {product.price}</li>
+						<li>
+							Status: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
+						</li>
+						<li>
+							Qty:{" "}
+							<select
+								value={qty}
+								onChange={(e) => {
+									setQty(e.target.value);
+								}}
+							>
+								{[...Array(product.countInStock).keys()].map((x) => (
+									<option key={x + 1} value={x + 1}>
+										{x + 1}
+									</option>
+								))}
+							</select>
+						</li>
+						<li>
+							{product.countInStock > 0 && (
+								<button onClick={handleAddToCart} className="button primary">
+									Add to Cart
+								</button>
+							)}
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductScreen;
