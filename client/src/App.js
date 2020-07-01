32ec306a89cc73e3493d6c896baa2bca3937e654
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-01 06:15:38 +0530

    added basic sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,61 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 function App() {
-	return <h1>working</h1>;
+	const openMenu = () => {
+		document.querySelector(".sidebar").classList.add("open");
+		document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	};
+	const closeMenu = () => {
+		document.querySelector(".sidebar").classList.remove("open");
+		document.body.style.backgroundColor = "white";
+	};
+
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="header">
+					<div className="brand">
+						<button onClick={openMenu}>&#9776;</button>
+						<Link className="nav-link" to="/">
+							amazon
+						</Link>
+					</div>
+					<div className="header-links">
+						<Link className="nav-link" to="/cart">
+							Cart
+						</Link>
+						<Link className="nav-link" to="/sign-in">
+							Sign In
+						</Link>
+					</div>
+				</header>
+				<aside className="sidebar">
+					<h3>Shopping Categories</h3>
+					<button className="sidebar-close-button" onClick={closeMenu}>
+						x
+					</button>
+					<ul>
+						<li>
+							<Link className="side-link" to="/pants">
+								Pants
+							</Link>
+						</li>
+
+						<li>
+							<Link className="side-link" to="/shirts">
+								Shirts
+							</Link>
+						</li>
+					</ul>
+				</aside>
+				<main className="main">
+					<div className="content">content</div>
+				</main>
+				<footer className="footer">All rights reserved</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
